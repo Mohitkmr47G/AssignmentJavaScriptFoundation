64202ff893430027dbcfbac7922263a8543c77ea
MohitKumar <120587670+Mohitkmr47G@users.noreply.github.com>
2023-08-16 21:17:10 +0530

    Update Q7.js

--- Q7.js
+++ Q7.js
@@ -1,43 +1,24 @@
-class ShoppingCart {
-  constructor() {
-    this.items = [];
-  }
+function removeDuplicatesFromCart(cart) {
+  const cartWithoutDuplicates = [];
 
-  addItem(item) {
-    const existingItem = this.items.find(cartItem => cartItem.id === item.id);
+  for (const item of cart) {
+    const existingItem = cartWithoutDuplicates.find(cartItem => cartItem.id === item.id);
     
-    if (existingItem) {
-      existingItem.quantity++;
-    } else {
-      this.items.push({ ...item, quantity: 1 });
+    if (!existingItem) {
+      cartWithoutDuplicates.push(item);
     }
   }
 
-  removeItem(item) {
-    const itemIndex = this.items.findIndex(cartItem => cartItem.id === item.id);
-
-    if (itemIndex !== -1) {
-      if (this.items[itemIndex].quantity > 1) {
-        this.items[itemIndex].quantity--;
-      } else {
-        this.items.splice(itemIndex, 1);
-      }
-    }
-  }
-
-  getCartItems() {
-    return this.items;
-  }
+  return cartWithoutDuplicates;
 }
 
 // Example usage
-const cart = new ShoppingCart();
+const customerCart = [
+  { id: 1, name: "Product A", price: 10 },
+  { id: 2, name: "Product B", price: 15 },
+  { id: 1, name: "Product A", price: 10 }, // Duplicate
+  { id: 3, name: "Product C", price: 20 },
+];
 
-const item1 = { id: 1, name: "Product A", price: 10 };
-const item2 = { id: 2, name: "Product B", price: 15 };
-
-cart.addItem(item1);
-cart.addItem(item2);
-cart.addItem(item1);
-
-console.log(cart.getCartItems());
+const cartWithoutDuplicates = removeDuplicatesFromCart(customerCart);
+console.log("Cart without duplicates:", cartWithoutDuplicates);
